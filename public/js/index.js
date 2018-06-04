ca65d2eb6a4cc528644b501bd96fffa916aebf55
Sreesha S <sreesha.s@bookmyshow.com>
2018-06-04 16:21:06 +0530

    Add form for messages and show incoming messages in broswer

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,27 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message) {
     console.log('New Message',message);
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    jQuery('#messages').append(li);
 })
 
-socket.emit('createMessage',{
-    from: 'Frank',
-    text: 'Hi'
-},function(response){
-    console.log('Got it',response);
+// socket.emit('createMessage',{
+//     from: 'Frank',
+//     text: 'Hi'
+// },function(response){
+//     console.log('Got it',response);
+// })
+
+jQuery('#message-form')
+.on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function(){
+
+    })
 })
--- server/server.js
+++ server/server.js
@@ -21,13 +21,13 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message,callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from,message.to));
+        // io.emit('newMessage', generateMessage(message.from,message.text));
         callback('this is from the server');
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect', () => {
